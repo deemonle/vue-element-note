1cc4d60d91cd38eb41e8d205cdcab64bcb84df41
daimeng <daimeng@users.noreply.github.com>
2018-09-18 19:30:33 +0800

    change helloworld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,18 @@
 <template>
-  <el-container>
-    <el-header>Header</el-header>
-    <el-container>
-      <el-aside width="200px">Aside</el-aside>
-      <el-main>Main</el-main>
-    </el-container>
-
   <el-menu default-active="2" mode="horizontal">
     <el-row>
       <el-col :xs="12" :sm="6" :md="4" :lg="3" :xl="1">
         <div class="grid-content bg-purple">
-          a'aaa
+          aaaa
         </div>  
-        </el-col>
+      </el-col>
       <el-col :xs="0" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple-light"></div></el-col>
       <el-col :xs="0" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
       <el-col :xs="12" :sm="6" :md="4" :lg="3" :xl="1"><div class="grid-content bg-purple-light"></div></el-col>
     </el-row>
     <el-menu-item index="1">处理中心</el-menu-item>
     <el-menu-item index="2">处理中心</el-menu-item>
-    <el-submenu index="3">
-      
-    </el-submenu>
-    
   </el-menu>
-  </el-container>
 </template>
   
 
