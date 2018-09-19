d52769b137523d74c3ef4f8de45c469fb50476df
daimeng <daimeng@users.noreply.github.com>
2018-09-19 18:17:49 +0800

    修改主页样式

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,70 +1,170 @@
 <template>
   <div>
-    <el-menu class="el-menu-demo" mode="horizontal">
-      <el-row>
-        <el-col :xs="12" :sm="6" :md="4" :lg="3" :xl="1">
-          <div class="grid-content bg-purple">
-            <el-menu-item index="1">Home</el-menu-item>
-          </div>
-        </el-col>
-        <el-col :xs="0" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple-light"></div></el-col>
-        <el-col :xs="0" :sm="6" :md="8" :lg="9" :xl="11"><div class="grid-content bg-purple"></div></el-col>
-        <el-col :xs="12" :sm="6" :md="4" :lg="3" :xl="1">
-          <div class="grid-content bg-purple-light">
-            <el-submenu index="2">
-              <template slot="title">Cont</template>
-              <el-menu-item index="2-1">选项1</el-menu-item>
-              <el-menu-item index="2-2">选项2</el-menu-item>
-              <el-menu-item index="2-3">选项3</el-menu-item>
-            </el-submenu>
-          </div>
-        </el-col>
-      </el-row>
+    <div class="header">
+      <div class="header-left">
+        <span>Note</span>  
+      </div>
+      <div class="header-right">
+        <span>mulu</span>  
+      </div>
+      
+    </div>
+    <div class="dropdown-content">
+      <div class="dropdown-content-item">JavaScript</div>
+      <div class="dropdown-content-item">CSS</div>
+      <div class="dropdown-content-item">Html</div>
+      <div class="dropdown-content-item">Vue.js</div>
+      <div class="dropdown-content-item">Others</div>
+      <div class="dropdown-content-item">GitHub</div>
+      <input class="dropdown-content-input" placeholder="请输入需要搜索的内容">
+    </div>
+    <div class="block">
+      <el-carousel height="150px">
+        <el-carousel-item v-for="item in 4" :key="item">
+          <h3>{{ item }}</h3>
+        </el-carousel-item>
+      </el-carousel>
+    </div>
 
-      
-      
-    </el-menu>
-    <el-row :gutter="20">
-  <el-col :span="6"><div class="grid-content bg-purple hidden-xs-only"></div></el-col>
-  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
-</el-row>
+
+    <div class="card">
+      <div class="card-title1">
+        aa
+      </div>
+      <div class="card-title2">
+        aa
+      </div>
+      <div class="card-title3">
+        aa
+      </div>
+    </div>
+    <div class="card">
+
+    </div>
+    <div class="card">
+
+    </div>
+    <div class="card">
+
+    </div>
+    <div class="card">
+
+    </div>
+
   </div>
 </template>
   
 
 <script>
-  export default {
-    data() {
-      return {
-        activeIndex: '1',
-        activeIndex2: '1'
-      };
-    },
-    methods: {
-      handleSelect(key, keyPath) {
-        console.log(key, keyPath);
-      }
-    }
-  }
+  
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  body{
-    margin: 0;
+  @mixin main-color{
+    background-color: #0a64a4;
   }
-  .bg-purple-dark {
-    background: #99a9bf;
+
+  .header{
+    height: 3.5rem;
+    width: 100%;
+    display: flex;
+    position: fixed;
+    font-size: 1.5rem;
+    flex-direction: row;
+    justify-content: space-between;
+    @include main-color;
+    color: #fff;
+    &-left {
+      padding-left: 1.5rem;
+      padding-top: 1rem;
+      &:hover{
+        color: #65a5d2;
+      }
+    }
+    &-right {
+      padding-right: 1.5rem;
+      padding-top: 1rem;
+      &:hover{
+        color: #65a5d2;
+      }
+    }
   }
-  .bg-purple {
-    background: #99a9bf;
+  .dropdown-content{
+    width: 100%;
+    display: none;
+    position: fixed;
+    min-height: 0rem;
+    flex-direction: column;
+    @include main-color;
+    color: #fff;
+    overflow: hidden;
+    // animation: content-animation-close .8s;
+    &-item {
+      padding-left: 1.5rem;
+      padding-top: 1rem;
+      height: 1.8rem;
+      text-align: left;      
+    }
+    &-input{
+      border: .1rem;
+      border-radius: .2rem;
+      font-size: 1rem;
+      padding: 0 .5rem;
+      height: 2rem;
+      margin: 1rem 1.5rem;         
+    }
   }
-  .bg-purple-light {
-    background: #99a9bf;
+
+  @keyframes content-animation-open {
+    from{max-height: 0rem;}
+    to{max-height: 15rem;}
   }
-  .grid-content {
-    min-height: 36px;
+  @keyframes content-animation-close {
+    from{max-height: 10rem;}
+    to{max-height: 0rem;}
   }
+  .carousel{
+    margin-top: 5rem !important; 
+  }
+  .el-carousel__item h3 {
+      color: #475669;
+      font-size: 14px;
+      opacity: 0.75;
+      line-height: 150px;
+      margin: 0;
+    }
+
+    .el-carousel__item:nth-child(2n) {
+    }
+    
+    .el-carousel__item:nth-child(2n+1) {
+    }
+  .card{
+    border-bottom: 1px solid #eeeeee;
+    height: 10rem;
+    flex: 1;
+    display: flex;
+    flex-direction: row;
+    width: 90%;
+    margin: 0 5%;
+    // @include main-color;
+    background-color: #ffffff;
+    &-title1{
+      flex: 1;
+      height: 2rem;
+      font-size: 2rem;
+    }
+    &-title2{
+      flex: 1;
+      height: 2rem;
+      font-size: 1.5rem;
+    }
+    &-title3{
+      flex: 1;
+      height: 2rem;
+      font-size: 1rem;
+    }
+  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+import 'element-ui/lib/theme-chalk/display.css'
 import '../static/css/reset.css'
 // Vue.use(Element, { size: 'small', zIndex: 3000 })
 Vue.config.productionTip = false
