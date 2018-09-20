9a03612f39c2cdd6d716e5f3ff59a15b71a334c0
daimeng <daimeng@users.noreply.github.com>
2018-09-20 19:54:03 +0800

    search

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,31 @@
 <template>
-  <div>
+  <div class="body">
     <div class="header">
       <div class="header-left">
-        <span>Note</span>  
+        <span>Note</span>
       </div>
       <div class="header-right">
-        <span>mulu</span>  
-      </div>
-      
+        <i class="iconfont iconfont-size-2">&#xe601;</i>
+      </div>  
     </div>
-    <div class="dropdown-content">
-      <div class="dropdown-content-item">JavaScript</div>
-      <div class="dropdown-content-item">CSS</div>
-      <div class="dropdown-content-item">Html</div>
-      <div class="dropdown-content-item">Vue.js</div>
-      <div class="dropdown-content-item">Others</div>
-      <div class="dropdown-content-item">GitHub</div>
-      <input class="dropdown-content-input" placeholder="请输入需要搜索的内容">
+
+    <div class="search">
+      <div class="search-input">
+        <i class="iconfont iconfont-size-1-2 search-input-icon">&#xe632;</i>
+      </div>   
     </div>
+    
+    <div class="dropdown-content"> 
+      <router-link :to="'home'">
+        <div class="dropdown-content-item">JavaScript<i class="iconfont iconfont-size-1">&#xe64e;</i></div>
+      </router-link>
+      <div class="dropdown-content-item">CSS<i class="iconfont iconfont-size-1">&#xe64e;</i></div>
+      <div class="dropdown-content-item">Html<i class="iconfont iconfont-size-1">&#xe64e;</i></div>
+      <div class="dropdown-content-item">Vue.js<i class="iconfont iconfont-size-1">&#xe64e;</i></div>
+      <div class="dropdown-content-item">Others<i class="iconfont iconfont-size-1">&#xe64e;</i></div>
+      <div class="dropdown-content-item">GitHub<i class="iconfont iconfont-size-1">&#xe64e;</i></div>
+    </div>
+    
     <div class="block">
       <el-carousel height="150px">
         <el-carousel-item v-for="item in 4" :key="item">
@@ -61,61 +69,118 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  @mixin main-color{
-    background-color: #0a64a4;
+  @mixin bg-color{
+    background-color: #ffffff;
+  }
+  
+  body{
+    width: 100%;
   }
 
+  @mixin border-style{
+    border-bottom: 1px solid #eeeeee;
+  }
+
+  @mixin hover-color{
+    color: #2ebaae;
+  }
+
+  @mixin header-font-color{
+    color: #222;
+  }
+  @mixin content-font-color{
+    color: #555;
+  }
+  
+  .iconfont-size-1{
+    font-size: 1rem;
+  }
+
+  .iconfont-size-1-2{
+    font-size: 1.5rem;
+  }
+
+  .iconfont-size-2{
+    font-size: 2rem;
+  }
+
+  .search{
+    margin: 1.5rem 1.5rem .5rem 1.5rem;
+    display: flex;
+    height: 3rem;
+    &-input{
+      flex: 1;
+      height: 2rem;
+      display: flex;
+      flex-direction: row-reverse;
+      padding-left: .5rem;
+      border: 1px solid #eeeeee;
+      border-radius: .2rem;
+      font-size: .9rem;   
+      &-icon{
+        padding: .25rem .25rem;
+        width: 2rem;
+        &:hover{
+          @include hover-color;
+        }
+      }
+    }  
+  }
   .header{
-    height: 3.5rem;
+    height: 3rem;
     width: 100%;
     display: flex;
-    position: fixed;
-    font-size: 1.5rem;
+    // position: fixed;
+    font-size: 1.4rem;
     flex-direction: row;
     justify-content: space-between;
-    @include main-color;
-    color: #fff;
+    @include border-style;
+    @include bg-color;
+    @include header-font-color;
     &-left {
-      padding-left: 1.5rem;
-      padding-top: 1rem;
+      padding: .8rem 1.5rem;
       &:hover{
-        color: #65a5d2;
+        @include hover-color;
       }
     }
     &-right {
-      padding-right: 1.5rem;
-      padding-top: 1rem;
+      padding: .5rem 1.5rem;
+      border-left: 1px solid #eeeeee;
       &:hover{
-        color: #65a5d2;
+        @include hover-color;
       }
     }
   }
   .dropdown-content{
     width: 100%;
-    display: none;
-    position: fixed;
+    display: flex;
+    // position: fixed;
     min-height: 0rem;
     flex-direction: column;
-    @include main-color;
-    color: #fff;
-    overflow: hidden;
-    // animation: content-animation-close .8s;
+    font-size: .9rem;
+    @include bg-color;
+    @include content-font-color;
     &-item {
-      padding-left: 1.5rem;
-      padding-top: 1rem;
-      height: 1.8rem;
-      text-align: left;      
+      margin: 0 1.5rem;
+      padding: .6rem .5rem;
+      display: flex;
+      justify-content: space-between;
+      @include border-style;
+      text-align: left;  
+      &:hover{
+        @include hover-color;
+      }  
     }
     &-input{
       border: .1rem;
       border-radius: .2rem;
-      font-size: 1rem;
+      font-size: .9rem;
       padding: 0 .5rem;
       height: 2rem;
-      margin: 1rem 1.5rem;         
+      margin: 2rem 1.5rem 1rem 1.5rem;
+      border: 1px solid #eeeeee;     
     }
   }
-
   @keyframes content-animation-open {
     from{max-height: 0rem;}
     to{max-height: 15rem;}
@@ -148,7 +213,7 @@
     flex-direction: row;
     width: 90%;
     margin: 0 5%;
-    // @include main-color;
+    // @include bg-color;
     background-color: #ffffff;
     &-title1{
       flex: 1;
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'element-ui/lib/theme-chalk/display.css'
 import '../static/css/reset.css'
+import './assets/style/iconfont.css'
 // Vue.use(Element, { size: 'small', zIndex: 3000 })
 Vue.config.productionTip = false
 Vue.use(ElementUI)
